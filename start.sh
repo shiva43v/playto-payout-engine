5af06d63bf602ce04df5d1c9a7d0f4f8af3adedf
shiva43v <cs21b055@iittp.ac.in>
2026-04-29 12:26:36 +0530

    Optimize start.sh to limit memory usage for free tier

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 # Change to the backend directory
 cd backend || exit
 
-# Start Celery worker in the background
-celery -A config worker -l info &
+# Start Celery worker in the background (using solo pool to save memory)
+celery -A config worker -l info -P solo --concurrency=1 &
 
 # Start Celery beat in the background
 celery -A config beat -l info &
 
-# Start the web server in the foreground
-gunicorn config.wsgi:application
+# Start the web server in the foreground (limit to 1 worker to save memory)
+gunicorn config.wsgi:application --workers 1 --threads 2
